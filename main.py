64b13ca6853c3917da6cebfa7456b30ec5596119
amithsbhat <bhatamith25@gmail.com>
2022-12-27 16:30:53 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+### Sample python script for jenkins pipeline ###
+import os
+x = [1,2,3,4,5,6,7]
+x.reverse()
+print("### Reversed list is {0} ###".format(x))
+
+y = [7,5,1,3,4,6,2]
+y.sort()
+print("### Sorted list is {0} ### ".format(y))
